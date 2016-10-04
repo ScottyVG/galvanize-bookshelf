0b6f236aaedff9bdc5a813d62500e66a5ad058dc
Scott Van Gilder <svangilder@gmail.com>
2016-10-04 17:15:10 -0600

    testing part 3

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,45 @@
 'use strict';
 
+const knex = require('../knex');
 const express = require('express');
+const humps = require('humps');
+const bcrypt = require('bcrypt');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  let {
+    email,
+    password
+  } = req.body;
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then(() => {
+      return bcrypt.hashSync(req.body.password, 12);
+    })
+    .then((hashedPassword) => {
+      let {
+        firstName,
+        lastName
+      } = req.body;
+      let insertNewUser = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        hashedPassword: hashedPassword
+      }
+      return knex('users')
+        .insert(humps.decamelizeKey(insertNewUser), '*')
+      res.send(humps.camelizeKeys(insertNewUsers))
+    })
+    .then((rows) => {
+      let user = humps.camelizeKeys(rows[0]);
+      delete user.hashedPassword;
+      req.session.userId = user.id;
+      res.send(user)
+    })
+})
 
 module.exports = router;
