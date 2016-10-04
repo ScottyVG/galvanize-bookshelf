b0ece3abc9d02809412450413a106d203961f19e
Scott Van Gilder <svangilder@gmail.com>
2016-10-04 15:42:14 -0600

    testing part 2

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,91 @@
 'use strict';
-
+const knex = require('../knex');
 const express = require('express');
-
+const humps = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res) => {
+  knex('books')
+    .orderBy('title')
+    .then((books) => {
+      res.send(humps.camelizeKeys(books));
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  knex('books')
+    .insert(humps.decamelizeKeys({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }))
+    .returning('*')
+    .then((books) => {
+      res.send(humps.camelizeKeys(books[0]));
+    });
+})
+
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+      const updateBook = {}
+      if (title) {
+        updateBook.title = req.body.title;
+      }
+      if (author) {
+        updateBook.author = req.body.author;
+      }
+      if (genre) {
+        updateBook.genre = req.body.genre;
+      }
+      if (description) {
+        updateBook.description = req.body.description;
+      }
+      if (coverUrl) {
+        updateBook.coverUrl = req.body.coverUrl;
+      }
+      return knex('books')
+        .update(humps.decamelizeKeys(updateBook))
+        .returning('*')
+        .where('id', req.params.id);
+    })
+    .then((books) => {
+      res.send(humps.camelizeKeys(books[0]));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  let book;
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if (!row) {
+        return next();
+      }
+      book = humps.camelizeKeys(row);
+      return knex('books')
+        .del()
+        .where('id', req.params.id);
+    })
+    .then(() => {
+      delete book.id;
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
